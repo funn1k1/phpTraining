fe91e07f958f7a76690111454be296b0cb581d13
Eugene <yukovich01@mail.ru>
2020-09-07 23:49:24 +0200

    Version1.0

---
+++ data.php
@@ -0,0 +1,265 @@
+<?php
+return [
+    'links'=>['About us','Typography','Contact us'],
+    'colMd10'=>[
+        'title'=>'The smartest and the most flexible Bootstrap template',
+        'text'=>'Create exactly what you need with our powerful Bootstrap toolkit.',
+        'buttons'=>[
+            'textBtn1'=>'Buy Intense Now',
+            'textBtn2'=>'Start a Journey',
+        ]
+    ],
+    'colSm6'=>[
+        'title1'=>'Novi Builder',
+        'title2'=>'Start building visually',
+        'text'=>'Easily change any element of Intense with the best visual drag and drop builder.',
+    ],
+
+    'slickSlides'=>[
+        'h6'=>'Create',
+        'slickNames'=>[
+            'text1'=>'Websites',
+            'text2'=>'Sliders',
+            'text3'=>'Content blocks',
+        ],
+        'texts'=>[
+            'text1'=>'Intense gives you a variety of powerful options, useful tools, and unlimited designs to easily create a website of your dreams that your clients will love.',
+        'text2'=>'Use the built-in slider plugins to create appealing blocks with smooth sliders that are completely responsive and customizable.',
+        'text3'=>'Change the layout of Intense with the included content blocks ranging from counters to tabs and accordions and other impressive elements.',
+    ]
+    ],
+    'containerFluid'=>[
+        'h1'=>'Check out all themes available in the full version of Intense',
+        'blocks'=>[
+            [
+                'img'=>'images/layouts/layout-1-336x450.jpg',
+                'text'=>'Home — Default',
+            ],
+            [
+                'img'=>'images/layouts/layout-2-336x450.jpg',
+                'text'=>'Home — Business',
+            ],
+            [
+                'img'=>'images/layouts/layout-3-336x450.jpg',
+                'text'=>'Home — Consulting',
+            ],
+            [
+                'img'=>'images/layouts/layout-4-336x450.jpg',
+                'text'=>'Home — Portfolio',
+            ],
+            [
+                'img'=>'images/layouts/layout-5-336x450.jpg',
+                'text'=>'Home — Studio',
+            ],
+            [
+                'img'=>'images/layouts/layout-6-336x450.jpg',
+                'text'=>'Home — App',
+            ],
+            [
+                'img'=>'images/layouts/layout-7-336x450.jpg',
+                'text'=>'Home — Event',
+            ],
+            [
+                'img'=>'images/layouts/layout-8-336x450.jpg',
+                'text'=>'Home — Blog',
+            ],
+            [
+                'img'=>'images/layouts/layout-9-336x450.jpg',
+                'text'=>'Home — Store',
+            ],
+            [
+                'img'=>'images/layouts/layout-10-336x450.jpg',
+                'text'=>'Home — Minimal',
+            ]
+        ]
+    ],
+    'colXs6'=>[
+        [
+            'img'=>'blurb-icon ith-layers',
+            'title'=>'60+ reusable elements',
+            'text'=>'Intense has a set of more than 60 UI elements designed to make your website attractive and feature-rich.'
+        ],
+        [
+            'img'=>'blurb-icon ith-font',
+            'title'=>'Unlimited Google fonts',
+            'text'=>'The template includes a vast range of fonts from Google Fonts library that can be easily switched and replaced.'
+        ],
+        [
+            'img'=>'blurb-icon ith-effect',
+            'title'=>'Various hover effects and animations',
+            'text'=>'Thanks to stunning animations and effects, a website built with Intense will always impress your visitors.'
+        ],
+        [
+            'img'=>'blurb-icon ith-pages',
+            'title'=>'Blog and shop pages',
+            'text'=>'Post your latest news and integrate eCommerce features with our pre-designed blog & shop pages.'
+        ]
+    ],
+    'rowsXl70'=>[
+        [
+            'img'=>'blurb-icon ith-devices',
+            'title'=>'Bootstrap 4 framework',
+            'text'=>'Intense has a set of more than 60 UI elements designed to make your website attractive and feature-rich.'
+        ],
+        [
+            'img'=>'blurb-icon ith-settings',
+            'title'=>'HTML and CSS compiling by Pug and Sass',
+            'text'=>'To simplify and improve the development of your website, Intense uses Pug and Sass code compiling.'
+        ],
+        [
+            'img'=>'blurb-icon ith-console',
+            'title'=>'Fully component approach to code',
+            'text'=>'We used a component approach to code to make the usage of Intense easier for all other developers.'
+        ]
+    ],
+    'rows30'=>[
+        [
+            'img'=>'blurb-icon ith-devices',
+            'title'=>'Responsive Layout',
+            'text'=>'All pages of Intense are completely responsive guaranteeing that your website will look great on any device.'
+        ],
+        [
+            'img'=>'blurb-icon ith-pages',
+            'title'=>'Rich UI Kit',
+            'text'=>'Our template supports full customization due to the rich UI kit with lots of blocks and sections.'
+        ],
+    ],
+    'container'=>[
+        'text'=>'Read the most popular reviews submitted by our customers.',
+        'btnText'=>'More Testimonials',
+    ],
+    'colsLg4'=>[
+        [
+            'text'=>'Intense is full of great details and very useful tools. The template code is clear and easy-to-edit. Moreover, it is well-documented and supported.',
+            'img'=>'images/persons/person-01-64x64.jpg',
+            'name'=>'Kevin Davis'
+        ],
+        [
+            'text'=>'It is a great template with everything my website needed. It took me a few hours to figure all out but it was worth the investment.',
+            'img'=>'images/persons/person-02-64x64.jpg',
+            'name'=>'Paul Sanders',
+        ],
+        [
+            'text'=>'This template has lots of preconfigured child themes, effects, and components. It is a great choice if you need a customizable website.',
+            'img'=>'images/persons/person-03-64x64.jpg',
+            'name'=>'Helen Wright',
+        ]
+    ],
+    'colLg8'=>[
+        'title1'=>'Intense is exactly what you need',
+        'title2'=>'Check out an incredible set of features provided by Intense.',
+        'btnTxt'=>'Buy Intense Now'
+    ],
+    'colsMd3'=>[
+        [
+        'img'=>'blurb-icon ith-devices',
+        'text'=>'Based on Bootstrap 4'
+        ],
+      [
+          'img'=>'blurb-icon ith-slider',
+          'text'=>'Sliders and carousels'
+      ],
+    [
+        'img'=>'blurb-icon ith-parallax',
+        'text'=>'Parallax'
+    ],
+    [
+        'img'=>'blurb-icon ith-layout-header',
+        'text'=>'Lots of header layouts'
+    ],
+    [
+        'img'=>'blurb-icon ith-layout-footer',
+        'text'=>'A variety of footer layouts'
+    ],
+    [
+        'img'=>'blurb-icon ith-smile',
+        'text'=>'Icon fonts'
+    ],
+        [
+            'img'=>'blurb-icon ith-video',
+            'text'=>'Video background',
+        ],
+        [
+            'img'=>'blurb-icon ith-layout',
+            'text'=>'26 child themes'
+        ]
+],
+    'colsMd4'=>[
+        [
+            'img'=>'blurb-icon icon-secondary ith-slider',
+            'text'=>'Revolution slider'
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-carousel',
+            'text'=>'Owl carousel'
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-calendar',
+            'text'=>'Owl carousel'
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-mail',
+            'text'=>'Mail form'
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-recaptcha',
+            'text'=>'ReCaptcha',
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-monkey',
+            'text'=>'Mailchimp',
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-ball',
+            'text'=>'Animate CSS, WOW animation',
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-search',
+            'text'=>'Search form'
+        ],
+        [
+            'img'=>'blurb-icon icon-secondary ith-gallery',
+            'text'=>'Gallery (Isotope) (hover effects)'
+        ],
+    ],
+    'colMd5'=>[
+        'title'=>'A wide variety of child themes in the full version',
+        'text'=>'Choose from a variety of Child themes in the paid version of Intense and use any of them for your website.'
+    ],
+    'colSm11'=>[
+        'text'=>'Trying to stay on top of it all? Get the best tools, resources and inspiration sent to your inbox weekly.',
+        'btnTxt'=>'Subscribe',
+        'inpTxt'=>'Enter your email address'
+    ],
+    'colLg5'=>[
+        'img1'=>'images/logo-default-161x27.svg',
+        'img2'=>'images/logo-inverse-161x27.svg',
+        'text'=>'Get the most for your website with Intense Free Multipurpose Website Template, valued by over 3,000 customers worldwide. With a variety of components, elements, and blocks, this responsive Bootstrap 4 template is definitely worth your attention.'
+    ],
+    'colMd11'=>[
+        'text'=>'178 West 27th Street, Suite 527, New York NY 10012, United States',
+        'numbers'=>[
+        'num1'=>'+1 234 567 8901',
+        'num2'=>'+1 234 567 8902'
+        ],
+        'listDays'=>[
+            'li1'=>'Mon-Fri: 8am - 6pm',
+            'li2'=>'Sat-Sun: 8am - 4pm',
+            'li3'=>'Holidays: closed'
+        ],
+        'listEmails'=>[
+            'email1'=>'mail@demolink.org',
+            'email2'=>'info@demolink.org'
+        ]
+    ],
+    'footer'=>[
+        'icons'=>[
+            'youtube'=>'icon icon-xs icon-social int-youtube novi-icon',
+            'facebook'=>'icon icon-xs icon-social int-facebook novi-icon',
+            'instagram'=>'icon icon-xs icon-social int-instagram novi-icon',
+            'twitter'=>'icon icon-xs icon-social int-twitter novi-icon',
+            'linkedin'=>'icon icon-xs icon-social int-linkedin novi-icon',
+        ]
+    ]
+]
+?>
